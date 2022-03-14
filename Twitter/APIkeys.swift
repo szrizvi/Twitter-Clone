405cbb435b8f354f2ea43ae4f137214f52f400ec
szrizvi <77036574+szrizvi@users.noreply.github.com>
2022-03-13 18:02:01 -0700

    Initial Commit

---
+++ Twitter/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  Twitter
+//
+//  Created by Zainab Rizvi on 13/03/2022.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let consumerKey = "TsGNPOfsFysUI0CfN3J1LtXlr"
+let consumerSecret = "4anEkaX2u2tRmdl9OKogEamsaPm7xJmTRVBkdq5jsp0MM03ZPT"
